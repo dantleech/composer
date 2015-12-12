0999180fc1db418ca68a86327dcef5d2b5551814
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-12-12 15:51:14 +1100

    Uncommented out parts of `DebugFormatter`

--- src/Composer/IO/WorkTracker/Formatter/DebugFormatter.php
+++ src/Composer/IO/WorkTracker/Formatter/DebugFormatter.php
@@ -26,23 +26,21 @@ class DebugFormatter implements FormatterInterface
 
     public function complete(AbstractWorkTracker $workTracker)
     {
-        //$this->formatMessage($workTracker, 'DONE  ', $workTracker->getTitle());
+        $this->formatMessage($workTracker, 'DONE  ', $workTracker->getTitle());
     }
 
     public function ping(AbstractWorkTracker $workTracker)
     {
-        /*
         $this->formatMessage($workTracker, 'PING  ', sprintf(
             '[#%06d] [%ss] %s',
             $workTracker->getPingCount(),
             $workTracker->getElapsedPingTime(),
             $workTracker->getTitle()
-        ));*/
+        ));
     }
 
     private function formatMessage(AbstractWorkTracker $workTracker, $subject, $message)
     {
-        //var_dump($workTracker->getDepth());
         $this->output->writeln(sprintf('%s%s: %s', str_repeat('  ', $workTracker->getDepth()), $subject, $message));
     }
 }
